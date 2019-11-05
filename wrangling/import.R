786467230c98595e63d159d63dc628ad29a9ccdb
Ethan Go <ethango@bu.edu>
2019-11-05 00:26:10 -0500

    finished importing data's from url and using readr functionalities

--- wrangling/import.R
+++ wrangling/import.R
@@ -1,4 +1,5 @@
 library(dslabs)
+library(tidyverse)
 
 #the system.file loeads data automatically from the dslabs package
 path <- system.file("extdata", package="dslabs")
@@ -14,3 +15,37 @@ file.copy(fullpath, getwd())
 
 #checks whether the  file is in our wd
 file.exist(filename)
+
+
+#look at the file
+read_lines(filename, n_max =3 )
+
+
+dat <- read_csv(filename)
+
+#read.csv is different than read_csv in that it makes the object in a data.frame object but read_csv makes it on a tibble data type
+dat2 <- read.csv(filename)
+class(dat2)
+
+#because strings columns are turned into factors we need to correct this
+dat3 <- read.csv(filename, stringsAsFactors = FALSE)
+
+
+#downloading file from the internet
+url <- "https://raw.githubusercontent.com/rafalab/dslabs/master/inst/extdata/murders.csv"
+dat <- read_csv(url)
+#loval file
+download.file(url, "murders.csv")
+
+
+#creastes directory
+tempfile()
+tmp_filename <- tempfile()
+download.file(url, tmp_filename)
+dat <- read_csv(tmp_filename)
+file.remove(tmp_filename)
+
+
+url <- "http://mlr.cs.umass.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data"
+dat4 <- read_csv(url,colnames=F)
+
