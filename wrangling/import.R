76a6e6c95d44236d556cf0ecd05b2226a58c85ff
Ethan Go <ethango@bu.edu>
2019-11-10 01:50:43 -0500

    added stuff'

--- wrangling/import.R
+++ wrangling/import.R
@@ -145,3 +145,112 @@ tmp
 tmp2 <- unite(tmp, column_name, c(key,gender))
 
 spread(tmp2, column_name, value)
+
+
+install.packages("Lahman")
+library(Lahman)
+top <- Batting %>% 
+  filter(yearID == 2016) %>%
+  arrange(desc(HR)) %>%    # arrange by descending HR count
+  slice(1:10)    # take entries 1-10
+top %>% as_tibble()
+Master %>% as_tibble()
+
+top_names <- top %>% left_join(Master) %>% select(playerID, nameFirst, nameLast, HR)
+top_salary <- Salaries %>% filter(yearID == 2016) %>%
+  right_join(top_names) %>%
+  select(nameFirst, nameLast, teamID, HR, salary)
+
+top_names <- top %>% left_join(Master) %>% select(playerID)
+mod <- AwardsPlayers %>% filter(yearID == 2016)%>% select(playerID)
+setdiff(mod,top_names)
+
+
+
+# Web Scraping using tidyverse rvest
+library(rvest)
+url <- "https://en.wikipedia.org/w/index.php?title=Gun_violence_in_the_United_States_by_state&oldid=919733895"    # permalink
+h <- read_html(url)
+class(h)
+h
+
+
+tab <- h %>% html_nodes("table")
+tab <- tab[[2]]
+
+tab <- tab %>% html_table
+class(tab)
+
+tab <- tab %>% setNames(c("state", "population", "total", "murders", "gun_murders", "gun_ownership", "total_rate", "murder_rate", "gun_murder_rate"))
+head(tab)
+
+h <- read_html("http://www.foodnetwork.com/recipes/alton-brown/guacamole-recipe-1940609")
+recipe <- h %>% html_node(".o-AssetTitle__a-HeadlineText") %>% html_text()
+prep_time <- h %>% html_node(".m-RecipeInfo__a-Description--Total") %>% html_text()
+ingredients <- h %>% html_nodes(".o-Ingredients__a-Ingredient") %>% html_text()
+
+guacamole <- list(recipe, prep_time, ingredients)
+guacamole
+
+
+get_recipe <- function(url){
+  h <- read_html(url)
+  recipe <- h %>% html_node(".o-AssetTitle__a-HeadlineText") %>% html_text()
+  prep_time <- h %>% html_node(".m-RecipeInfo__a-Description--Total") %>% html_text()
+  ingredients <- h %>% html_nodes(".o-Ingredients__a-Ingredient") %>% html_text()
+  return(list(recipe = recipe, prep_time = prep_time, ingredients = ingredients))
+}
+
+
+
+
+library(rvest)
+url <- "https://web.archive.org/web/20181024132313/http://www.stevetheump.com/Payrolls.htm"
+h <- read_html(url)
+
+
+nodes <- html_nodes(h, "table")
+html_text(nodes[[8]])
+html_table(nodes[[8]])
+
+getTable <- function(tab){
+  return(html_table(tab))
+}
+
+
+getTable(nodes[[1]])
+getTable(nodes[[2]])
+getTable(nodes[[3]])
+getTable(nodes[[4]])
+
+
+getTable(nodes[[19]])
+getTable(nodes[[20]])
+getTable(nodes[[21]])
+
+
+tab <- html_table(nodes[[10]])
+tab <- tab[ -c(1) ]
+tab <- tab %>% setNames(c("Team", "Payroll", "Average"))
+tab <- tab[-c(1),] 
+head(tab)
+
+tab_2 <- html_table(nodes[[19]])
+tab_2 <- tab_2 %>% setNames(c("Team", "Payroll", "Average"))
+tab_2 <- tab_2[-c(1),] 
+head(tab_2)
+
+full_join(tab, tab_2)
+
+
+url <- "https://en.wikipedia.org/w/index.php?title=Opinion_polling_for_the_United_Kingdom_European_Union_membership_referendum&oldid=896735054"
+h <- read_html(url)
+class(h)
+h
+
+
+tab_3 <- h %>% html_nodes("table")
+tab_3 <- tab_3[[5]]
+tab_3 <- tab_3 %>%  html_table(fill=TRUE)
+tab_3
+class(tab_3)
